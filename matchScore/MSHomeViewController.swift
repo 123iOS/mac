706c39ab36aa0e35ed5b5f8c153ca3e05375ebc3
My Repo <tyaa.mail@gmail.com>
2016-04-06 18:13:38 +0700

    creating base UI

---
+++ matchScore/MSHomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MSHomeViewController.swift
+//  matchScore
+//
+//  Created by Triyakom PT on 4/6/16.
+//  Copyright © 2016 Triyakom PT. All rights reserved.
+//
+
+import UIKit
+
+class MSHomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
